da798c1697e64fd8248f7d51b5f81cb2f597459d
lionheart <asaf.levari@gmail.com>
2024-12-21 22:36:10 +0200

    sql question

---
+++ common_friend .sql
@@ -0,0 +1,57 @@
+--Given a friendship table, find for each user his friends with whom they have at least 3 friends in common. 
+--Rank friends by the number of common friend. The table record one-way relationship: sender of friend request and recipient of friend request.
+
+
+INSERT INTO `Friendship` (`id`, `user_id`, `friend_id`) VALUES
+(1, 'alice', 'bob'),
+(2, 'alice', 'charles'),
+(3, 'alice', 'david'),
+(4, 'alice', 'mary'),
+(5, 'bob', 'david'),
+(6, 'bob', 'charles'),
+(7, 'bob', 'mary'),
+(8, 'david', 'sonny'),
+(9, 'charles', 'sonny'),
+(10, 'bob', 'sonny');
+
+select * from friendship limit 5;
++----+---------+-----------+
+| id | user_id | friend_id |
++----+---------+-----------+
+|  1 | alice   | bob       |
+|  2 | alice   | charles   |
+|  3 | alice   | david     |
+|  4 | alice   | mary      |
+|  5 | bob     | david     |
++----+---------+-----------+
+5 rows in set (0.00 sec)
+
+WITH all_friends AS (
+    SELECT user_id, friend_id
+    FROM friendship
+    UNION ALL
+    SELECT friend_id, user_id
+    FROM friendship
+),
+expand_friends AS (
+    SELECT 
+        all_friends_1.user_id AS a
+        ,all_friends_1.friend_id AS b
+        ,all_friends_2.friend_id AS a_friend
+        ,all_friends_3.friend_id AS b_friend
+
+    FROM all_friends AS all_friends_1
+    JOIN all_friends AS all_friends_2
+        ON all_friends_1.user_id = all_friends_2.user_id
+    JOIN all_friends AS all_friends_3
+        ON all_friends_1.friend_id = all_friends_3.user_id
+        AND all_friends_3.friend_id = all_friends_2.friend_id
+)
+
+SELECT a, b, COUNT(*)
+FROM expand_friends
+GROUP BY 1,2
+HAVING common_friend >= 3
+
+
+
